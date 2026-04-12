d199fec09d81deabbc62d2680c1e5628c7fec8b6
Konrad Brodowski <konrad@iMac-Konrad-Be.local>
2026-04-12 16:35:58 +0200

    fix db host localhost

--- api.php
+++ api.php
@@ -5,7 +5,7 @@ header('Access-Control-Allow-Methods: GET, POST, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type');
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') { exit(0); }
 
-$host = 'srv2069.hstgr.io';
+$host = 'localhost';
 $db   = 'u869576174_bistro';
 $user = 'u869576174_centrala';
 $pass = 'BRoda2026!';
